e64514963fc7682d61a56d55d6699450d700509c
Woojin Kil <woojinkil@Woojins-MacBook-Pro.local>
2024-01-17 22:08:46 -0600

    chat

---
+++ badgerEvents2/backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios")
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "36e0df4b-0edc-44fe-b3fc-0fad45e5cdad"}}
+
+    )
+    return res.status(r.status).json(r.data)
+  } catch (e) {
+    if (e.response) {
+        // Handle the error if 'e.response' exists
+        return res.status(e.response.status).json(e.response.data);
+      } else {
+        // Handle the error if 'e.response' is undefined
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+  }
+});
+
+app.listen(3001);
+
---
+++ badgerEvents2/frontend/src/ChatsPage.jsx
@@ -0,0 +1,15 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={"374509a7-b480-47c6-952a-27994cc06678"}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
